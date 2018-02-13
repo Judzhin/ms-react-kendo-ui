ee1eee28e7ad7fcbdd1f6978c1dbd39f2aa61016
Judzhin Miles <info@msbios.com>
2018-02-13 23:41:51 +0200

    Lazy Composer Commiter

--- getting_started.sh
+++ getting_started.sh
@@ -7,6 +7,12 @@ npm i -g npm
 
 # create-react-app <project-name>
 
+# kendo components
 npm i @progress/kendo-buttons-react-wrapper
 npm i @progress/kendo-dateinputs-react-wrapper
 npm i @progress/kendo-inputs-react-wrapper
+npm i @progress/kendo-charts-react-wrapper
+npm i @progress/kendo-ui
+npm install --save @progress/kendo-theme-default
+
+npm i bootstrap
