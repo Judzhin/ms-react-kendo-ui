f45d54923789d857b4665126dfe3cfaf82414424
Judzhin Miles <info@msbios.com>
2018-02-13 23:01:22 +0200

    Lazy Composer Commiter

--- generator.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-npm i -g create-react-app
-npm i -g npm
-
-# create-react-app <project-name>
---
+++ getting_started.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+npm i -g create-react-app
+npm i -g npm
+
+# create-react-app <project-name>
+
+npm i @progress/kendo-buttons-react-wrapper
+npm i @progress/kendo-dateinputs-react-wrapper
+npm i @progress/kendo-inputs-react-wrapper
